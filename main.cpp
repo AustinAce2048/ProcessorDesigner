206bb8aaae0e456446f889e88c5da6f127966976
Austin <austinwprogramming@gmail.com>
2022-09-08 15:35:56 -0700

    Added custom pixel control, whole screen is blue

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <GLFW/glfw3.h>
 
+const int screenWidth = 1280;
+const int screenHeight = 720;
+const char windowName[] = "Processor Designer";
+
+//Each pixel has a position and 3 values for RGB
+float *pixels = new float[screenWidth * screenHeight * 3];
+
+
+
+void draw () {
+    //Make all pixels blue
+    for (int i = 0; i < screenWidth; i++) {
+        for (int j = 0; j < screenHeight; j++) {
+            pixels[(j + screenHeight * i) * 3] = 0.0f;
+            pixels[(j + screenHeight * i) * 3 + 1] = 0.0f;
+            pixels[(j + screenHeight * i) * 3 + 2] = 1.0f;
+        }
+    }
+}
+
+
+
 int main (void) {
     GLFWwindow* window;
 
@@ -8,8 +30,8 @@ int main (void) {
         return -1;
     }
 
-    //Create a window and set OpenGL context
-    window = glfwCreateWindow (1280, 720, "Processor Designer", NULL, NULL);
+    //Create a window and set the OpenGL context
+    window = glfwCreateWindow (screenWidth, screenHeight, windowName, NULL, NULL);
     if (!window) {
         glfwTerminate ();
         return -1;
@@ -20,8 +42,13 @@ int main (void) {
 
     //Loop until window is closed
     while (!glfwWindowShouldClose (window)) {
-        //Render
+        //Render here
         glClear (GL_COLOR_BUFFER_BIT);
+
+        draw ();
+
+        //Draw pixels to frame buffer (Khronos has amazing documentation)
+        glDrawPixels (screenWidth, screenHeight, GL_RGB, GL_FLOAT, pixels);
 
         //Swap front and back buffers
         glfwSwapBuffers (window);
@@ -30,6 +57,9 @@ int main (void) {
         glfwPollEvents ();
     }
 
+    //Memory cleanup
     glfwTerminate ();
+    delete[] pixels;
+
     return 0;
 }
