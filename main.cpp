788bf90783f25b3f925ad8c03b004117bdb2f121
Austin <austinwprogramming@gmail.com>
2022-09-09 14:07:39 -0700

    Draw a square, ugly colors

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <GLFW/glfw3.h>
+#include <Windows.h>
 
-const int screenWidth = 1280;
-const int screenHeight = 720;
+const int screenWidth = 1920;
+const int screenHeight = 1080;
 const char windowName[] = "Processor Designer";
+
+const int squareSize = 50;
 
 //Each pixel has a position and 3 values for RGB
 float *pixels = new float[screenWidth * screenHeight * 3];
@@ -10,13 +13,29 @@ float *pixels = new float[screenWidth * screenHeight * 3];
 
 
 void draw () {
-    //Make all pixels blue
-    for (int i = 0; i < screenWidth; i++) {
-        for (int j = 0; j < screenHeight; j++) {
-            pixels[(j + screenHeight * i) * 3] = 0.0f;
-            pixels[(j + screenHeight * i) * 3 + 1] = 0.0f;
-            pixels[(j + screenHeight * i) * 3 + 2] = 1.0f;
+    //Draw centered square and then fill in all other pixels
+    //Loop scans from bottom to top, left to right (One row at a time)
+    for (int i = 0; i < screenHeight; i++) {
+        for (int j = 0; j < screenWidth; j++) {
+            if (i >= (screenHeight / 2) - squareSize && i <= (screenHeight / 2) + squareSize && j >= (screenWidth / 2) - squareSize && j <= (screenWidth / 2) + squareSize) {
+                pixels[(j + i * screenWidth) * 3] = 0.0f;
+                pixels[(j + i * screenWidth) * 3 + 1] = 0.0f;
+                pixels[(j + i * screenWidth) * 3 + 2] = 1.0f;
+            } else {
+                pixels[(j + i * screenWidth) * 3] = 0.0f;
+                pixels[(j + i * screenWidth) * 3 + 1] = 1.0f;
+                pixels[(j + i * screenWidth) * 3 + 2] = 0.0f;
+            }
         }
+    }
+}
+
+
+
+//Creates a callback to listen for the escape key, it will work on press down or up (down in this case)
+void KeyCallback (GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose (window, GL_TRUE);
     }
 }
 
@@ -32,6 +51,8 @@ int main (void) {
 
     //Create a window and set the OpenGL context
     window = glfwCreateWindow (screenWidth, screenHeight, windowName, NULL, NULL);
+    //Force window to be fullscreen on the main monitor
+    glfwSetWindowMonitor (window, glfwGetPrimaryMonitor (), 0, 0, screenWidth, screenHeight, 60);
     if (!window) {
         glfwTerminate ();
         return -1;
@@ -39,6 +60,9 @@ int main (void) {
 
     //Set window context to current
     glfwMakeContextCurrent (window);
+
+    //Grabs the callback set earlier, only runs if pollEvents is running
+    glfwSetKeyCallback (window, KeyCallback);
 
     //Loop until window is closed
     while (!glfwWindowShouldClose (window)) {
